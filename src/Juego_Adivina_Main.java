0fd18183308f2e3c760f2b1a094b116fdff56b0f
Alba Garcia Via <albagarcia@iesbalmasedabhi.com>
2026-02-07 12:33:17 +0100

    Añadida validación de rango para el número introducido y comentarios

--- src/Juego_Adivina_Main.java
+++ src/Juego_Adivina_Main.java
@@ -10,11 +10,23 @@ public class AdivinaNumero {
         System.out.print("Adivina un numero del 1 al 10 RAPIDO!: ");
         int intento = scanner.nextInt();
 
+        //Validar numero de intentos.
+        if (intento < 1 || intento > 10) {
+            System.out.println("Numero fuera de rango");
+            return;
+
+        //Si el numero se ha acertado
         if (intento == numeroSecreto) {
             System.out.println("Genial!! Has acertado :)!");
+        //Si no se ha acertado
         } else {
             System.out.println("Ohhh! El numero no es correcto...);
             System.out.println("El numero correcto era: " + numeroSecreto);
+        }    
+            //Cierre de scanner
+            scanner.close();
         }
     }
-}
+        
+     
+        
